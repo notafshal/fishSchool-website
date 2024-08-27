7e48a0f1ea9d6408eac1a57ce85764b979a2da51
Afshal <afshal56@gmail.com>
2024-08-27 16:33:15 +0545

    about us page done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,62 @@
 import Image from "next/image";
 import gallery1 from "@/assets/footerImage/gallery1.jpg";
+import about from "@/assets/aboutmain.jpg";
+
 export default function About() {
   return (
     <>
-      {" "}
-      <div className="flex flex-col font-normal mt-3 ">
-        <p className="text-center mx-auto">Awards</p>
-        <Image
-          className="w-24 hover:w-32 cursor-pointer transition-all"
-          src={gallery1}
-          alt="gallery1"
-        />
+      <div className="mx-8 my-8 bg-slate-100 p-5 rounded-lg flex flex-row">
+        <div className="w-1/2">
+          <Image src={about} alt="mainabout" className="rounded-lg " />
+        </div>
+        <div className="w-1/2 flex  flex-col justify-center">
+          <h2 className="text-center font-semibold text-teal-700 my-2">
+            WHO ARE WE?
+          </h2>
+          <p className="font-light">
+            Fish School Nepal is a specialized fish store in Kirtipur,
+            Kathmandu, offering a wide selection of freshwater fish for aquarium
+            enthusiasts. The store caters to both aggressive and peaceful
+            species, offering a variety of fish and aquarium accessories. Fish
+            School Nepal also provides custom tank design services, ensuring
+            unique tank setups fit the customers space and enhance the overall
+            look of their home or office. The store also offers professional
+            aquascaping services, ensuring a visually captivating and
+            sustainable aquatic environment. Fish School Nepal is a community
+            hub for aquatic enthusiasts, offering advice and tips to help
+            customers achieve their aquarium goals. The store is a favorite
+            among local fish enthusiasts, making it a trusted destination for
+            quality fish, custom aquariums, and expert aquascaping..
+          </p>
+        </div>
+      </div>
+      <div className="mx-8 my-8 bg-slate-100 p-5 rounded-lg flex flex-col">
+        <div>
+          <h1 className="text-center font-semibold text-teal-700 mx-auto">
+            Our Achievements
+          </h1>
+        </div>
+        <div className="flex flex-row font-normal mt-3 ">
+          <Image
+            className=" w-52 mx-5 cursor-pointer rounded-lg"
+            src={gallery1}
+            alt="gallery1"
+          />
+          <p className="mx-5 font-light px-28  ">
+            Fish School Nepal, a leading ornamental fish retailer in Nepal, has
+            secured second place in the Betta competition, specifically in the
+            Plakat variety category. The Plakat Betta, known for its robust
+            build and vibrant colors, is a popular choice among Betta
+            enthusiasts. The stores second position in this category reflects
+            its commitment to high standards of care, breeding, and selection.
+            The fish presented by Fish School Nepal displayed exceptional
+            physical traits and demonstrated the stores dedication to
+            maintaining the health and vitality of their stock. This recognition
+            solidifies Fish School Nepals position as a trusted source for
+            top-quality ornamental fish in Kathmandu and beyond, enhancing its
+            credibility and customer trust.{" "}
+          </p>
+        </div>
       </div>
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,16 @@ export default function Navbar() {
   return (
     <>
       <div className="bg-teal-700 w-dvw h-16 p-2 flex flex-row justify-between">
-        <div className="flex flex-row">
-          <Image
-            className="h-10 w-10 rounded-full"
-            src={logo}
-            alt="Fishschool logo"
-          />
-          <h3 className="text-white mx-4 my-2">Fish School Nepal</h3>
-        </div>
+        <Link href="/">
+          <div className="flex flex-row">
+            <Image
+              className="h-10 w-10 rounded-full"
+              src={logo}
+              alt="Fishschool logo"
+            />
+            <h3 className="text-white mx-4 my-2">Fish School Nepal</h3>
+          </div>
+        </Link>
         <ul className="flex flex-row w-72 p-2 justify-between text-white">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
